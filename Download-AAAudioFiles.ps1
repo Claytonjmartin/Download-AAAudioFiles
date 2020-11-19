fb86df62eefb92d25adba46b527cea5ed30695d2
claytonmartin <47785508+claytonmartin@users.noreply.github.com>
2020-11-19 16:55:12 -0500

    Add comments

--- Download-AAAudioFiles.ps1
+++ Download-AAAudioFiles.ps1
@@ -1,5 +1,19 @@
 Function Download-AAAudioFiles {
-    param(
+        <#
+    Description:
+        This function will download previously uploaded audio files from the specified Microsoft Teams Auto Attendant to the specified folder on the local machine.
+    Parameters:
+        -Identitiy
+        -OutputFolder
+    Example:
+        Download-AAAudioFiles -Identitiy "6f45e634-0e8d-4f00-8f32-d8d4ca18dcfa" -OutputFolder "C:\Users\adm\Downloads\queue1"
+    Author:
+        Clayton Martin
+    Source: 
+        https://github.com/Claytonjmartin/Download-AAAudioFiles
+    Disclamer: 
+        This script is provided "As Is" without any warranty of any kind. In no event shall the author be liable for any damages arising from the use of this script.
+    #>param(
         [Parameter(Mandatory = $true)]$Identity,
         [Parameter(Mandatory = $true)]$OutputFolder
     )
