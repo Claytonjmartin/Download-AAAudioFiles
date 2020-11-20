e76dcaec4090555efacb8d3014d8e2639c6ba865
claytonmartin <47785508+claytonmartin@users.noreply.github.com>
2020-11-20 09:33:19 -0500

    Additional Comments

--- Download-AAAudioFiles.ps1
+++ Download-AAAudioFiles.ps1
@@ -6,12 +6,12 @@ Function Download-AAAudioFiles {
         -Identitiy
         -OutputFolder
     Example:
-        Download-AAAudioFiles -Identitiy "6f45e634-0e8d-4f00-8f32-d8d4ca18dcfa" -OutputFolder "C:\Users\adm\Downloads\queue1"
+        Download-AAAudioFiles -Identitiy "6f45e634-0e8d-4f00-8f32-d8d4ca18dcfa" -OutputFolder "C:\Users\adm\Downloads\AA1"
     Author:
         Clayton Martin
     Source: 
         https://github.com/Claytonjmartin/Download-AAAudioFiles
-    Disclamer: 
+    Disclaimer: 
         This script is provided "As Is" without any warranty of any kind. In no event shall the author be liable for any damages arising from the use of this script.
     #>param(
         [Parameter(Mandatory = $true)]$Identity,
